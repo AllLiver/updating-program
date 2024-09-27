fff670cc563a1b949dcf024dba3fb73f0add7081
AllLiver <oliverfedd@outlook.com>
2024-09-26 20:32:56 -0600

    more testing

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,19 @@ async fn main() -> Result<()> {
     if let Some(current_version_remote) = remote_cargo_toml.iter().find(|x| x.starts_with("version = ")) {
         let current_version_remote = current_version_remote.split("=").collect::<Vec<&str>>()[1].trim().trim_start_matches('"').trim_end_matches('"');
 
-        println!("{}", current_version_remote);
+        let current_version_local: Vec<u8> = current_version_local.split(".").map(|x| x.parse::<u8>().expect("Failed to parse remote version")).collect();
+        let current_version_remote: Vec<u8> = current_version_remote.split(".").map(|x| x.parse::<u8>().expect("Failed to parse remote version")).collect();
+
+        let mut out_of_date = true;
+
+        for i in 0..current_version_local.len() {
+            if current_version_local >= current_version_remote {
+                out_of_date = false;
+                break;
+            }
+        }
+
+        println!("are we out of date? {}", out_of_date);
     } else {
         return Err(anyhow!("Failed to retrieve current version on main remote branch"));
     }
